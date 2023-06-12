013a74c745b0485f717897087c52dc41ab7b2c3d
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2023-06-12 11:02:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  // if(html.classList.contains('light')){
+  //   html.classList.remove('light')
+  // } else{
+  //   html.classList.add('light')
+  // }
+  html.classList.toggle("light")
+
+  //pegar a tag da img
+  const img = document.querySelector("#profile img")
+
+  //substituir a img
+  if(html.classList.contains("light")){
+    img.setAttribute("src", './assets/avatar-light.png')
+  } else{
+    img.setAttribute("src","./assets/avatar.png")
+  }
+}
